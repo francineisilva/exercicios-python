125a66673ef8317c38f53b89896ab0e550322227
Francinei Silva <francineisilva7@gmail.com>
2023-03-16 21:01:23 -0300

    ex001.py
    Programa escreve a mensagem olá, mundo na tela!

---
+++ ex001.py
@@ -0,0 +1,8 @@
+# Tipos Primitivos (Str, Float, Int, Bool) - Cadeia, Caracter, Real, Inteiro e Lógico
+#Toda variável ela precisa ter um tipo. 
+
+#Comando de Entrada: INPUT 
+#Comando de Saída: PRINT (Imprima)
+#Toda vez que um texto estiver dentro de aspas "", isso quer dizer que é uma STRING, ou seja, e um texto definido pelo DEV.
+
+print("Olá, Mundo!")
